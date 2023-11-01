973c094eb59263a82d48d910564db876eba387c2
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-11-01 18:33:20 +0000

    numpy,pandas,matplotlib

---
+++ simple_libraries.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# NumPy
+array = np.array([1, 2, 3])
+print(array)
+
+# Pandas
+data = {'Name': ['Alice', 'Bob'], 'Age': [25, 30]}
+df = pd.DataFrame(data)
+print(df)
+
+# Matplotlib
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
