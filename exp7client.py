e08c676f1e14ee6ea0b6934a004999c4e67ce6d8
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-11-06 15:07:08 +0530

    Create exp7client.py

---
+++ exp7client.py
@@ -0,0 +1,16 @@
+import socket
+
+# Set up the client
+HOST = '127.0.0.1'
+PORT = 12345
+
+# Create a UDP socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Read the file
+with open('hii.txt', 'rb') as f:
+    data = f.read()
+
+# Send the file
+s.sendto(data, (HOST, PORT))
+print("File has been sent successfully.")
