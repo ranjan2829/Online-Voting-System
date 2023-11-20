905a6849f9b20f9edad2363b079cae161b5fc33d
Ranjan <73215366+ranjan2829@users.noreply.github.com>
2023-11-20 22:11:10 +0530

    Create AI12.py

---
+++ AI12.py
@@ -0,0 +1,51 @@
+def reset():
+    global board, counter
+    board = [[0] * n for _ in range(n)]
+    counter = 0
+
+def display(board):
+    global counter
+    counter += 1
+    for row in board:
+        print(" ".join(map(str, row)))
+    print()
+
+def check(board, row, column):
+    for i in range(column):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(column, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, n), range(column, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def marker(board, column):
+    global counter
+    possibility = False
+
+    if column == n:
+        display(board)
+        return True
+
+    for i in range(n):
+        if check(board, i, column):
+            board[i][column] = 1
+            possibility = marker(board, column + 1)
+            board[i][column] = 0
+
+    return possibility
+
+n = int(input("Enter the size of the board: "))
+counter = 0
+reset()
+
+if not marker(board, 0) and counter == 0:
+    print("No feasible solution exists for the given dimensions")
+else:
+    print("There are a total of " + str(counter) + " possibilities for the given dimensions!")
